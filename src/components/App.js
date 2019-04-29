626ddff4d41c7ddd36b95cf50388628c246965b5
anthonygharvey <anthony@anthonygharvey.com>
2019-04-29 18:52:31 -0400

    Added Axios and integrated Unsplash API
    Added Axios to handle network requests
    Retrieve and log Unsplash API requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = searchTerm => {
-    console.log(`Logged from App: ${searchTerm}`);
-  };
+  async onSearchSubmit(searchTerm) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: searchTerm },
+      headers: {
+        Authorization:
+          "Client-ID 47ff7b8c5ff4476e2afeaa166884a8e649e47ed830cab9b25a4e74abc741d8a8"
+      }
+    });
+    // .then(res => {
+    //   console.log(res.data.results);
+    // });
+    console.log(response.data.results);
+  }
 
   render() {
     return (
